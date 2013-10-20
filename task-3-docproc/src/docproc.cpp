b1002bd0f5e7172c4de5b4b351d1782f6304867a
Anton Grigoryev <me@ansgri.com>
2013-10-21 02:29:51 +0400

    NEW: task-3-docproc: document processing task scaffolding

---
+++ task-3-docproc/src/docproc.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <algorithm>
+#include <stdexcept>
+#include <opencv2/opencv.hpp>
+#include "docproc.h"
+#include "utils.h"
+
+
+namespace rsdt { namespace docproc {
+
+// TODO: implement functions from docproc.h
+
+}}
